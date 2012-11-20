d71eec36b72a2431bd8547bd8beded7c71af484a
侯宜安 <lthouyin@gmail.com>
2012-11-20 14:55:09 +0800

    创建人、学生类
    注意继承时构造函数的一种新用法。

---
+++ 培训/DotNet开发技能课程/面相对象基础知识教案/课后练习示例/StudentSort/StudentSort/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSort
+{
+    /// <summary>
+    /// 人
+    /// 其实在此示例中，只有学生，没有老师，没有必要写几个基类“人”。
+    /// 示例为了演示继承，而写个person。
+    /// </summary>
+    class Person
+    {
+        
+        string _name;
+
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public Person()
+        {
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="name">姓名</param>
+        public Person(string name)
+        {
+            _name = name;
+        }
+    }
+}
