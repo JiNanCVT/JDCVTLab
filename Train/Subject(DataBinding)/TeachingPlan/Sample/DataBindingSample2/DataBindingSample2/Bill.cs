b1f015edf2bd749c443646c5b9b0f1446fbbeb7e
侯宜安 <lthouyin@gmail.com>
2012-11-24 02:25:34 +0800

    数据绑定的两个示例

---
+++ Train/Subject(DataBinding)/TeachingPlan/Sample/DataBindingSample2/DataBindingSample2/Bill.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingSample2
+{
+    public class Bill
+    {
+        private int _price;
+
+        public int Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        private int _quantity;
+
+        public int Quantity
+        {
+            get { return _quantity; }
+            set { _quantity = value; }
+        }
+
+
+        public int Total
+        {
+            get { return _price * _quantity; }
+        }
+
+    }
+}
---
+++ Train/Subject(DataBinding)/TeachingPlan/Sample/DataBindingSample3/DataBindingSample3/Bill.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingSample3
+{
+    public class Bill:INotifyPropertyChanged
+    {
+        private int _price;
+
+        public int Price
+        {
+            get { return _price; }
+            set 
+            {               
+                _price = value;
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Total"));
+            }
+        }
+
+        private int _quantity;
+
+        public int Quantity
+        {
+            get { return _quantity; }
+            set 
+            {
+                _quantity = value; 
+                if(PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("Total"));
+            }
+        }
+
+
+        public int Total
+        {
+
+            get { return _price * _quantity; }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
