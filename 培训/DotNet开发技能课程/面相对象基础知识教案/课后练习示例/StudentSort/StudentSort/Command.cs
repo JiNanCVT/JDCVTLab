47c203e285f08e9857beef85267757001b2b77b0
XiaoqingTan <txqdoudou@163.com>
2012-11-24 14:49:14 +0800

    d

---
+++ 培训/DotNet开发技能课程/面相对象基础知识教案/课后练习示例/StudentSort/StudentSort/Command.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSort
+{
+    /// <summary>
+    /// 命令的基类
+    /// </summary>
+    abstract public class Command
+    {
+
+        protected List<string> _helpMessage;
+
+        /// <summary>
+        /// 执行命令
+        /// </summary>
+        private void ExecuteCommand()
+        {
+           //显示提示信息
+            ShowAllHelpMessage();
+            try
+            {
+                Execute();
+            }
+            //如果执行命令出错，从新执行命令
+            catch (CommandErrorException ce)
+            {
+                Console.WriteLine(ce.Message);
+                ExecuteCommand();
+            }
+
+        }
+
+        /// <summary>
+        /// 执行命令
+        /// </summary>
+        public virtual bool Execute()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ShowAllHelpMessage()
+        {
+            foreach (var m in _helpMessage)
+            {
+                Console.WriteLine(m);
+            }
+        }
+
+
+        public class CommandErrorException : Exception
+        {
+        }
+       
+    }
+}
---
+++ 培训/DotNet开发技能课程/面相对象基础知识教案/课后练习示例/StudentSort/StudentSort/Student.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentSort
+{
+    /// <summary>
+    /// 学生类
+    /// </summary>
+    class Student:Person
+    {
+        private int _score;
+
+        /// <summary>
+        /// 分数
+        /// </summary>
+        public int Score
+        {
+            get { return _score; }
+            set { _score = value; }
+        }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        public Student()
+        {
+        }
+        /// <summary>
+        /// 注意base这种用法
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="score"></param>
+        public Student(string name, int score):base(name)
+        {
+            _score = score;
+        }
+    }
+}
